5ae47ff85ee5db2b5ad3201312f483354f7539dd
Taiki Kawata <taiki.kawata020427@gmail.com>
2026-03-03 20:14:00 +0900

    演算子と制御構文怪しい場所

--- src/basics/Main.java
+++ src/basics/Main.java
@@ -1,7 +1,22 @@
 package basics;
 
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Java学習スタート！");
+
+        //equalsメソッドにnullを渡すとNullPointerExceptionが発生する
+        Object a = new Object();
+        Object b = null;
+        System.out.println(a.equals(b));
+
+        //swich文条件式が戻せる型は int, byte, short, enum, String, char のみ
+        //long, float, double, boolean は使用できない
+        
+        int c = 1;
+        for (int i = 2, total = 0; i <= 5; i++) {
+            total += c * i;
+        }
+        //System.out.println(total); for文ので宣言した変数のスコープはfor文の外では使用できない
     }
 }
